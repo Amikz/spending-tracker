19f42ba0e0dfaae71687d120e245d147f9a02e4c
Kim <kimberlyawills@hotmail.com>
2021-11-10 17:59:22 -0600

    Remove unwanted characters from fields, e.g. letters from number field

--- js/spending-tracker.js
+++ js/spending-tracker.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
-	
-	$('.transactionRepeat').hide();
+    addEditPages();
+});
+
+function addEditPages() {
+    jQuery("#addEditPages").parsley({ excluded: "input[type=button], input[type=submit], input[type=reset], input[type=hidden], [disabled], :hidden" });
+    
+    $('.transactionRepeat').hide();
 	$('#transactionRepeat_Check').click(function() {
         $(".transactionRepeat").toggle();
 		if ($("#transactionRepeat_Check").is(':checked')) {
@@ -9,7 +14,7 @@ $(document).ready(function() {
             $('div.addEditTransactions').css('grid-template-rows', 'repeat(4, [inputField] max-content) [buttons] auto [end]');
 		}
 	});
-
+    
     $('.categoryBudget').hide();
     $('#setCategoryBudget').click(function() {
         $('.categoryBudget').toggle();
@@ -20,19 +25,71 @@ $(document).ready(function() {
             $('div.addEditCategories').css('grid-template-rows', 'repeat(3, [inputField] max-content) [buttons] auto [end]');
         }
     });
-
+    
     $('.addEditTransactions').hide();
-    $('.addEditCategories').show();
+    $('.addEditCategories').show(); //TODO: change to hide
 
-    $('#addTransactionWithinCategory').click(function() {
-        $('.addEditCategories').hide();
-        $('.addEditTransactions').show();
+    $('#saveButton').click(function() {
+        $('#addEditPages').parsley().validate();
+        if($("#addEditPages").parsley().isValid()) {
+            //Save the data
+        }
     });
-
+    
     $('#cancelButton').click(function() {
         if($('div.addEditTransactions').is(':visible')) { //TODO: add to this condition so that it also checks that we came from the Categories page
             $('.addEditTransactions').hide();
             $('.addEditCategories').show();
         }
     });
-});
+    
+    $('#addTransactionWithinCategory').click(function() {
+        $('.addEditCategories').hide();
+        $('.addEditTransactions').show();
+    });
+    
+    $('.nameFormat').on('input', function() {
+        var position = this.selectionStart;
+        var setPosition = (/([^(A-z|\-|'|À|Á|Â|Ã|Ä|Å|Æ|Ç|È|É|Ê|Ë|Ì|Í|Î|Ï|Ñ|Ò|Ó|Ô|Õ|Ö|Š|Ú|Û|Ü|Ù|Ý|Ÿ|Ž|à|á|â|ã|ä|å|æ|ç|è|é|ê|ë|ì|í|î|ï|ñ|ò|ó|ô|õ|ö|š|ù|ú|û|ü|ý|ÿ|ž)])|(`|\^|\(|\)|\[|\]|\\|\||_)/g).test($(this).val());
+
+        //Replace anything that isn't A-z, -, ', or accented letters
+        $(this).val($(this).val().replace(/([^(A-z|\-|'|À|Á|Â|Ã|Ä|Å|Æ|Ç|È|É|Ê|Ë|Ì|Í|Î|Ï|Ñ|Ò|Ó|Ô|Õ|Ö|Š|Ú|Û|Ü|Ù|Ý|Ÿ|Ž|à|á|â|ã|ä|å|æ|ç|è|é|ê|ë|ì|í|î|ï|ñ|ò|ó|ô|õ|ö|š|ù|ú|û|ü|ý|ÿ|ž)])|(`|\^|\(|\)|\[|\]|\\|\||_)/g, ''));
+
+        if(setPosition)
+            this.selectionEnd = position - 1;
+    });
+    
+    $('.wholeNumber').on('input', function() {
+        var position = this.selectionStart;
+        var setPosition = (/([^(0-9)])|(`|\^|\(|\)|\[|\]|\\|\||_)/g).test($(this).val());
+
+        //Replace anything that isn't 0-9
+        $(this).val($(this).val().replace(/([^(0-9)])|(`|\^|\(|\)|\[|\]|\\|\||_)/g, ''));
+
+        if(setPosition)
+            this.selectionEnd = position - 1;
+    });
+    
+    $(".monetaryAmount").on("input", function() {
+        var position = this.selectionStart;
+        var setPosition = (/([^(0-9|\.)])|(`|\^|\(|\)|\[|\]|\\|\||_)/g).test($(this).val());
+
+        //Replace anything that isn't 0-9 or .
+        $(this).val($(this).val().replace(/([^(0-9|\.)])|(`|\^|\(|\)|\[|\]|\\|\||_)/g, ''));
+
+        //Ensure there is only one decimal (at most)
+        var spot = $(this).val().indexOf('.');
+        if(spot != -1) {
+            var first = $(this).val().slice(0, spot + 1);
+            var second = $(this).val().slice(spot + 1);
+            second = second.replace(/\./g, '');
+            $(this).val(first + second);
+        }
+
+        //Prevent additional characters after 2 decimal places
+        $(this).val($(this).val().replace(/(^.+\.[0-9]{2})(.*)/, '$1'));
+
+        if(setPosition)
+            this.selectionEnd = position - 1;
+    });
+}
