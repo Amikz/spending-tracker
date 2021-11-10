2f4bb1d23eb5fbbbdcb9ba5a1ff6ae15276704f4
Kim <kimberlyawills@hotmail.com>
2021-11-09 22:12:25 -0600

    Adjusted layout formatting to use grid instead of flexbox

--- js/spending-tracker.js
+++ js/spending-tracker.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
 
-    $('.addEditCategories').hide();
+    $('.categoryBudget').hide();
+    $('#setCategoryBudget').click(function() {
+        $('.categoryBudget').toggle();
+
+        if($(this).is(":checked")) {
+            $('.addEditCategories').css('grid-template-rows', 'repeat(6, [inputField] max-content) [buttons] auto [end]');
+        } else {
+            $('.addEditCategories').css('grid-template-rows', 'repeat(3, [inputField] max-content) [buttons] auto [end]');
+        }
+    });
+
+    //$('.addEditCategories').hide();
 });
